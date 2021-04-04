307e62a1c11262e3a81bfe8a45fe2ab528124260
RamProg <ramirodlp@gmail.com>
2021-04-04 14:26:38 -0300

    first title, img and button

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'https://virtualhockey.com/wp-content/uploads/2021/01/vh_favicodark.png',
+        }}      />
+      <Text style={styles.title}>This is Virtual Hockey!</Text>
+      <Button
+        title="LOGIN"
+        color="blue"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  }
 });
