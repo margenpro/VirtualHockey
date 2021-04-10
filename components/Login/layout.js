106b1c42b55400d79929c52e92b7fd2bb7dac3e8
margenpro <64564587+margenpro@users.noreply.github.com>
2021-04-10 17:08:05 -0300

    style to log in

--- components/Login/layout.js
+++ components/Login/layout.js
@@ -1,43 +1,57 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 import styles from './styles'
+import { LinearGradient } from 'expo-linear-gradient'
 
-export function Layout ({userInputHandler, passInputHandler, submitHandler}) {
+export function Layout({ userInputHandler, passInputHandler, submitHandler }) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>User</Text>
-            <Input
-            onChangeText={userInputHandler}
-                placeholder='username'
-                leftIcon={
-                    <Icon
-                        name='user'
-                        size={24}
-                        color='black'
-                    />
-                }
-            />
-            <Text style={styles.title}>Password</Text>
-            <Input
-            onChangeText={passInputHandler}
-                placeholder='password'
-                leftIcon={
-                    <Icon
-                        name='key'
-                        size={24}
-                        color='black'
-                    />
-                }
-            />
-            <Button
-            onPress = {submitHandler}
-                title="Login"
-                color="blue"
-                accessibilityLabel="Learn more about this purple button"
-            />
-            <Text style={styles.title}>New User? Sign Up!</Text>
+            <LinearGradient style={styles.container} colors={['rgba(2, 28, 59, 1)', 'rgba(19, 38, 135, 1)']}>
+                <Image
+                    style={styles.logo}
+                    source={{
+                        uri: 'https://reactnative.dev/img/tiny_logo.png',
+                    }}
+                />
+                <Text style={styles.title}>USERNAME</Text>
+                <Input
+                    onChangeText={userInputHandler}
+                    placeholder='username'
+                    style={styles.input}
+                    leftIcon={
+                        <Icon
+                            name='user'
+                            size={24}
+                            color='white'
+                        />
+                    }
+                />
+                <Text style={styles.title}>PASSWORD</Text>
+                <Input
+                    onChangeText={passInputHandler}
+                    placeholder='password'
+                    secureTextEntry
+                    style={styles.input}
+                    leftIcon={
+                        <Icon
+                            name='key'
+                            size={24}
+                            color='white'
+                        />
+                    }
+                />
+                <TouchableOpacity
+                    onPress={submitHandler}
+                    accessibilityLabel="Learn more about this purple button"
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>LOG IN</Text>
+                </TouchableOpacity>
+                <Text style={styles.footer}>New User? Sign Up!</Text>
+            </LinearGradient>
+
         </View>
     );
 }
