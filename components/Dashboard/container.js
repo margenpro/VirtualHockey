69840e7fe9db6b22a33c16a85160c3d7ae2c1020
Alberto <albertomalek.ort@gmail.com>
2021-04-21 21:33:28 -0300

    datos de firebase

--- components/Dashboard/container.js
+++ components/Dashboard/container.js
@@ -16,6 +16,10 @@ export function Dashboard({ navigation }) {
   
     const db = firebase.firestore()
 
+    const [userName, setUserName] = useState("");
+    const [userPoints, setUserPoints] = useState("");
+
+
     const navigateToWorkouts = () => {
         console.log("voy a navegar")
         navigation.navigate("Workouts")
@@ -45,12 +49,33 @@ export function Dashboard({ navigation }) {
         }
     }
 
+
+    useEffect( () => {
+        const getCurrentUserData = async () => {
+            try {
+                let user = firebase.auth().currentUser
+                let doc = await db.collection("users").doc(user.uid).get()
+                let data = doc.data()
+                console.log(data)
+                setUserName(data.username)
+                setUserPoints(data.points)
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        }
+
+        getCurrentUserData()
+
+    }, [] );
+
     return (
         // <WorkoutsStack />
             <Layout
                 getProfileImage={getProfileImage}
                 navigateToWorkouts={navigateToWorkouts}
                 playLast={playLast}
+                userName={userName}
+                userPoints={userPoints}
             />
     );
 }
--- components/Dashboard/layout.js
+++ components/Dashboard/layout.js
@@ -3,12 +3,16 @@ import { Text, View, Image, TouchableOpacity, ScrollView, FlatList } from 'react
 // import { LinearGradient } from "expo-linear-gradient";
 import styles from "./styles";
 import { Frame } from '../Frame'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 
 export function Layout({
     getProfileImage,
     navigateToWorkouts,
-    playLast
+    playLast,
+    userName,
+    userPoints
 }) {
     return (
         <Frame>
@@ -19,12 +23,12 @@ export function Layout({
                         source={getProfileImage()} />
                 </View>
                 <View style={styles.usernameAndPosition}>
-                    <Text style={styles.username}>Elina </Text>
+                    <Text style={styles.username}>{userName} </Text>
                     <Text style={styles.username}># 1</Text>
                 </View>
                 <View style={styles.trophyAndRanking}>
                     <Text style={styles.username}>+ </Text>
-                    <Text style={styles.username}>2450</Text>
+                    <Text style={styles.username}> <Ionicons name={'trophy'} size={25} color={'#E9D41B'} /> {userPoints}</Text>
                 </View>
                 <View style={styles.explanationMessage}>
                     <Text style={styles.explanationMessageText}>Resume your workouts, earn points and get to the top of the </Text>
