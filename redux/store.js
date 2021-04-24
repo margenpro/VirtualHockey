d089607a07528d7d35a4fa28936697c510fe70e5
Ramiro De la. Pena <RPena@tagroupholdings.com>
2021-04-24 16:47:24 -0300

    working on redux

--- redux/store.js
+++ redux/store.js
@@ -3,6 +3,9 @@ import { reducers } from './reducers'
 
 export default () => {
     return {
-        ...createStore(reducers)
+        ...createStore(
+            reducers, /* preloadedState, */
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     }
 }
