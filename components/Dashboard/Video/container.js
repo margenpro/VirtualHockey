de113eb3d681bdc5adbf0541c9097fc2b096e7d6
margenpro <64564587+margenpro@users.noreply.github.com>
2021-04-26 13:43:44 -0300

    vimeo api test

--- components/Dashboard/Video/container.js
+++ components/Dashboard/Video/container.js
@@ -1,15 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Image, View } from "react-native";
 import { Layout } from "./layout";
+//import { Vimeo } from "vimeo"
 
 export function Video({ navigation, route }) {
 
     const getUrl = () => route.params.url
 
-        return (
-            <Layout
-                getUrl={getUrl}
-            />
-        );
+    return (
+        <Layout
+            getUrl={getUrl}
+        />
+    );
 }
+/*    ---Api de Vimeo-----
+    const [videoUri, setVideoUri] = useState('este no es el link')
+
+    useEffect(() => {
+        getVideo()
+        console.log(videoUri)
+    })
+
+    const getVideo = () => {
+        //TODO - No van aca las claves
+        let client = new Vimeo("ec3313f574ce286ab55a552d0831287181060180", "w5QjEEHeU8Bvbx3M6qqg+8tA3r6IqgXHg4/8HfWP22vlZYs0GsvVlVc6pd+pe0WDm/18WP/1HfbxPQmVoqpwJCyoc77eN2fD2LiHF6reCMUv7pO8hRL0P0xO70r17sXm", "4abbc0f7a99b7fee5d3707728e4785da");
+        client.request({
+            method: 'GET',
+            path: '/videos/541377453'
+        }, function (error, body, status_code, headers) {
+            if (error) {
+                console.log(error);
+            }
+            setVideoUri(body.link)
+        })
+    }
+    return (
+        <>
+            <Layout
+                videoUri={videoUri}
+            />
+        </>
+    );
+}
+*/
+
 
