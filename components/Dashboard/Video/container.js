fafbf35e84b62ab7292c8232939f47fd72c6fc10
margenpro <64564587+margenpro@users.noreply.github.com>
2021-05-08 14:25:51 -0300

    Video orientation fixed

--- components/Dashboard/Video/container.js
+++ components/Dashboard/Video/container.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Layout } from "./layout";
 import * as ScreenOrientation from "expo-screen-orientation"
+import { BackHandler } from 'react-native'
 
 export function Video({ navigation, route }) {
 
@@ -17,22 +18,10 @@ export function Video({ navigation, route }) {
     const exitFullScreen = () => {
         ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
     }
-
-    /*   const getVideo = () => {
-           //TODO - No van aca las claves
-           // let client = new Vimeo("ec3313f574ce286ab55a552d0831287181060180", "w5QjEEHeU8Bvbx3M6qqg+8tA3r6IqgXHg4/8HfWP22vlZYs0GsvVlVc6pd+pe0WDm/18WP/1HfbxPQmVoqpwJCyoc77eN2fD2LiHF6reCMUv7pO8hRL0P0xO70r17sXm", "4abbc0f7a99b7fee5d3707728e4785da");
-           // client.request({
-           //     method: 'GET',
-           //     path: '/videos/541377453',
-              
-           }, function (error, body, status_code, headers) {
-               if (error) {
-                   console.log(error);
-               }
-               console.log("Seteo el video")
-               setVideoUri(body)
-           })
-       }*/
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", exitFullScreen)
+        return () => BackHandler.removeEventListener("hardwareBackPress",exitFullScreen)
+    },[])
     return (
         <>
             <Layout
--- components/Dashboard/Video/layout.js
+++ components/Dashboard/Video/layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from "./styles";
 import { Frame } from '../../Frame'
-import { Audio, Video } from 'expo-av';
+import { Video } from 'expo-av';
 
 export function Layout({
     video,
     playFullScreen,
 }) {
+    //Esto tiene que ir en el container
     const urlVideo = "https://player.vimeo.com/external/475218949.hd.mp4?s=ba45e54d6ef6152a1837619dd9e4ce5fe8641ea0&profile_id=175"
     return (
-        
+
         <Video
             ref={video}
             style={styles.video}
@@ -18,8 +19,8 @@ export function Layout({
             }}
             useNativeControls
             resizeMode="contain"
-            onLoadStart={playFullScreen}
+            onLoadStart={playFullScreen}     
         />
-        
+
     );
 }
