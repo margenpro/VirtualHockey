89023d29fdbb75afb3894156dda567bac01ace1d
rpena <rpena@tagroupholdings.com>
2021-05-15 16:53:26 -0300

    payments on

--- fonts/index.js
+++ fonts/index.js
@@ -1,10 +1,11 @@
-import { useFonts } from 'expo-font';
-import * as Font from 'expo-font';
+import { useFonts } from "expo-font";
+import * as Font from "expo-font";
+import comfortaa from "../assets/fonts/Comfortaa-YJnL.ttf";
 
 export async function getFont() {
-    await Font.loadAsync({
-        comfortaa: require('../assets/fonts/Comfortaa-YJnL.ttf'),
-        comfortaaBold: require('../assets/fonts/ComfortaaBold-rmEK.ttf'),
-        comfortaaLight: require('../assets/fonts/ComfortaaLight-MJ0v.ttf'),
-    })
+  await Font.loadAsync({
+    comfortaa,
+    // comfortaaBold: require('../assets/fonts/ComfortaaBold-rmEK.ttf'),
+    // comfortaaLight: require('../assets/fonts/ComfortaaLight-MJ0v.ttf'),
+  });
 }
