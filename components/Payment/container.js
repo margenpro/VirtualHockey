8238e25c3bcca76855362e232a2cb273b7027bca
FriasSantiago <santiago.frias.diuk@gmail.com>
2021-06-25 13:51:59 -0300

    trycatch payment options

--- components/Payment/container.js
+++ components/Payment/container.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "firebase/auth";
 import { getStorage } from "../../firebase";
 import { useFirebaseApp } from "reactfire";
@@ -13,7 +13,7 @@ const Payment = ({ user, navigation, setUser }) => {
     name: "",
     city: "",
     country: "",
-    address: "",
+    address: ""
   });
 
   const [formCompleteMessage, setFormCompleteMessage] = useState(false);
@@ -28,7 +28,7 @@ const Payment = ({ user, navigation, setUser }) => {
   const inputHandler = (title, evt) => {
     setUserData({
       ...userData,
-      [title]: evt,
+      [title]: evt
     });
     setFormCompleteMessage(false);
   };
@@ -37,12 +37,21 @@ const Payment = ({ user, navigation, setUser }) => {
     for (let key in userData) if (userData[key] === "") return false;
     return true;
   };
-
-  Stripe.setOptionsAsync({
-    publishableKey:
-      "pk_test_51Iov1mH463ReSVuZ0ZoWrohc3XnW8R1kZNQkCpU8qU5SEQw5aeFxaTf2D8hfQGz6bpuhSUod5Bltl05QkVbeNOZg00ZGxGGbXf",
-    androidPayMode: "test",
-  });
+  useEffect(() => {
+    Alert.alert("Setenado opciones", "Entre al setOptions");
+    async function setOptions() {
+      try {
+        await Stripe.setOptionsAsync({
+          publishableKey:
+            "pk_test_51Iov1mH463ReSVuZ0ZoWrohc3XnW8R1kZNQkCpU8qU5SEQw5aeFxaTf2D8hfQGz6bpuhSUod5Bltl05QkVbeNOZg00ZGxGGbXf",
+          androidPayMode: "production"
+        });
+      } catch (error) {
+        Alert.alert("Error", error);
+      }
+    }
+    setOptions();
+  }, []);
 
   // const loadingHandler = () => {
   //   setLoading(!loading);
@@ -53,51 +62,60 @@ const Payment = ({ user, navigation, setUser }) => {
       setFormCompleteMessage(true);
       return null;
     }
-
-    const token = await Stripe.paymentRequestWithCardFormAsync();
-
-    setLoading(true);
-
-    token.card.name = userData.name;
-    token.card.country = userData.country;
-    token.card.addressCity = userData.city;
-    token.card.addressLine1 = userData.address;
-
+    let token = null;
     try {
-      let response = await fetch(
-        "https://us-central1-virtualhockey.cloudfunctions.net/app/api/pay",
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-          body: JSON.stringify({ token: token }),
-        }
-      );
-
-      let res = await response.json();
-
-      if (res.status === "succeeded") {
-        const paymentDate = new Date().toUTCString();
-        await makeMember(paymentDate);
-        setUser({ role: 2, paymentDate: paymentDate });
-        navigation.navigate("BottomTab");
-      }
+      token = await Stripe.paymentRequestWithCardFormAsync();
+      token.card.name = userData.name;
+      token.card.country = userData.country;
+      token.card.addressCity = userData.city;
+      token.card.addressLine1 = userData.address;
     } catch (error) {
-      Alert.alert(
-        "Oops!",
-        `An error has occurred! Please try again or contact administrator :(`
-      );
+      Alert.alert("Error", error);
     }
-    setLoading(false);
+    if (token) {
+      setLoading(true);
+
+      try {
+        let response = await fetch(
+          "https://us-central1-virtualhockey.cloudfunctions.net/app/api/pay",
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({ token: token })
+          }
+        );
+
+        let res = await response.json();
+
+        if (res.status === "succeeded") {
+          const paymentDate = new Date().toUTCString();
+          await makeMember(paymentDate);
+          setUser({ role: 2, paymentDate: paymentDate });
+          navigation.navigate("BottomTab");
+        }
+      } catch (error) {
+        Alert.alert(
+          "Oops!",
+          `An error has occurred! Please try again or contact administrator :(`
+        );
+      }
+      setLoading(false);
+    } else {
+      Alert.alert("Oops!", "An error has ocurred, please try again later");
+    }
   };
 
-  const makeMember = async (date) => {
-    await db.collection("users").doc(user.id).update({
-      role: 2,
-      paymentDate: date,
-    });
+  const makeMember = async date => {
+    await db
+      .collection("users")
+      .doc(user.id)
+      .update({
+        role: 2,
+        paymentDate: date
+      });
   };
 
   return (
@@ -111,14 +129,14 @@ const Payment = ({ user, navigation, setUser }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    user: state.userReducer.user,
+    user: state.userReducer.user
   };
 };
 
 const actionCreators = {
-  setUser: setterUserAction,
+  setUser: setterUserAction
 };
 
 export default connect(mapStateToProps, actionCreators)(Payment);
